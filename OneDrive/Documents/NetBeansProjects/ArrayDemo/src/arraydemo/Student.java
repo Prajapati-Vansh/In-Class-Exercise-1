ce8db20455ad944ec333efc130b726c1205762c0
Vansh3110 <prajvans@sheridancollege.ca>
2024-01-17 09:45:08 -0500

    First commit

---
+++ OneDrive/Documents/NetBeansProjects/ArrayDemo/src/arraydemo/Student.java
@@ -0,0 +1,27 @@
+package arraydemo;
+
+/**
+ *
+ * @author vsp31
+ */
+public class Student {
+    private int sId;
+    private String sName;
+
+    public int getsId() {
+        return sId;
+    }
+
+    public void setsId(int sId) {
+        this.sId = sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+    
+}
